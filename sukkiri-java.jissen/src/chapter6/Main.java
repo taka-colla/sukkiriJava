4f84a25f6e1c53ee522b8add4aab321c092fae01
takahashi <takahashi@colla-tech.co.jp>
2018-10-25 16:29:17 +0900

    FuncListクラス作成

--- sukkiri-java.jissen/src/chapter6/Main.java
+++ sukkiri-java.jissen/src/chapter6/Main.java
@@ -1,5 +1,16 @@
 package chapter6;
 
+import java.util.function.IntBinaryOperator;
+
 public class Main {
 
+	public static int sub(int a, int b) {
+		return a - b;
+	}
+	public static void main(String[] args) {
+		//「２つの引数の差を求める処理」の実体を生み出し、代入する
+		IntBinaryOperator func = (int a, int b) -> {	return a - b; };
+		int a = func.applyAsInt(5, 3);
+		System.out.println("5-3は"+a);
+	}
 }
