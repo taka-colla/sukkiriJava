a30c4109713dccd15726e02b0d3ccfeedb50d3f1
takahashi <takahashi@colla-tech.co.jp>
2018-10-25 17:01:49 +0900

    練習 6-1解答

--- sukkiri-java.jissen/src/chapter6/Main.java
+++ sukkiri-java.jissen/src/chapter6/Main.java
@@ -1,16 +1,21 @@
 package chapter6;
 
-import java.util.function.IntBinaryOperator;
 
-public class Main {
+	interface Func1{
+		boolean call(int x);
+	}
 
-	public static int sub(int a, int b) {
-		return a - b;
+	interface Func2{
+		String call(boolean male,String name);
 	}
-	public static void main(String[] args) {
-		//「２つの引数の差を求める処理」の実体を生み出し、代入する
-		IntBinaryOperator func = (int a, int b) -> {	return a - b; };
-		int a = func.applyAsInt(5, 3);
-		System.out.println("5-3は"+a);
-	}
+
+    public class Main{
+    	public static void main (String[] args) {
+    	FuncList funcList = new FuncList();
+    	Func1 f1 = FuncList::isOdd;
+    	Func2 f2 = funcList::addNamePrefix;
+    	System.out.println(f1.call(15));
+    	System.out.println(f2.call(true,"Smith"));
+
+    }
 }
