9d8370f24e7be0c9ca6b30b740179279dacac95a
takahashi <takahashi@colla-tech.co.jp>
2018-10-25 14:24:46 +0900

    練習 4-1解答

--- sukkiri-java.jissen/src/chapter4/Book.java
+++ sukkiri-java.jissen/src/chapter4/Book.java
@@ -1,5 +1,52 @@
 package chapter4;
 
-public class Book {
+import java.util.Date;
+
+public class Book implements Comparable<Book>,Cloneable{
+
+	private String title;
+	private Date publishDate;
+	private String comment;
+	public int hashCode() {
+		int result = 1;
+		result = result * 31 + title.hashCode();
+		result = result * 31 + publishDate.hashCode();
+		return result;
+	}
+
+	public boolean equals(Object o) {
+		if(o == this) {
+			return true;
+		}
+		if(o == null){
+			return false;
+		}
+		if(!(o instanceof Book)) {
+			return false;
+		}
+		Book book = (Book) o;
+		if(!this.title.equals(book.title)){
+			return false;
+		}
+		if(!this.publishDate.equlas(book.publishDate)) {
+			return false;
+		}
+		return true;
+	}
+
+	public int compareTo(Book b) {
+		return this.publishDate.compareTo(b.publishDate);
+	}
+
+	public Book clone() {
+		Book result = new Book();
+		result.title = this.title;
+		result.publishDate = (Date)this.publishDate.clone();
+		result.comment = this.comment;
+		return result;
+	}
+
+
+
 
 }
