9aeecb36d9e3eb986b2fe6bd1354e9bdc357dba6
takahashi <takahashi@colla-tech.co.jp>
2018-10-25 11:48:43 +0900

    練習 3-2の解答

--- sukkiri-java.jissen/src/chapter3/Main.java
+++ sukkiri-java.jissen/src/chapter3/Main.java
@@ -1,5 +1,21 @@
 package chapter3;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+		public static void main(String[] args) {
+	Hero saito = new Hero("斎藤");
+	Hero suzuki = new Hero("鈴木");
+	List<Hero> heroList = new ArrayList<Hero>();
+	heroList.add(saito);
+	heroList.add(suzuki);
+
+	for(int i = 0; i<heroList.size();i ++) {
+		System.out.println(heroList.get(i).getName());
+	}
+
+
+
+		}
 }
