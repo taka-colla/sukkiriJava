3f622b0014ccb889c6bea46de4a9015d7629b92a
takahashi <takahashi@colla-tech.co.jp>
2018-10-25 11:39:07 +0900

    練習3-2のHeroクラス作成

---
+++ sukkiri-java.jissen/src/chapter3/Hero.java
@@ -0,0 +1,15 @@
+package chapter3;
+
+public class Hero {
+
+	private String name;
+
+	public Hero(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+}
