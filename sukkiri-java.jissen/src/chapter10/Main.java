9e05fcacd9940298def40f5dfe194d3574289591
takahashi <takahashi@colla-tech.co.jp>
2018-10-29 12:31:55 +0900

    練習10-1 解答

--- sukkiri-java.jissen/src/chapter10/Main.java
+++ sukkiri-java.jissen/src/chapter10/Main.java
@@ -1,5 +1,18 @@
 package chapter10;
 
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.Properties;
+
 public class Main {
+	public static void main(String[] args) throws Exception {
+		Reader fr = new FileReader("c:\\pref.properties");
+		Properties p = new Properties();
+		p.load(fr);
+		String capital = p.getProperty("aichi.capital");
+		String food = p.getProperty("aichi.food");
+		System.out.println(capital + ":" + food);
+		fr.close();
+	}
 
 }
