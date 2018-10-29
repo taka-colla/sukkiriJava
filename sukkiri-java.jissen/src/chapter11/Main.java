5a70eae474f26478cf1083fda568e30e60b1a3b5
takahashi <takahashi@colla-tech.co.jp>
2018-10-29 15:47:22 +0900

    練習 11-2解答

--- sukkiri-java.jissen/src/chapter11/Main.java
+++ sukkiri-java.jissen/src/chapter11/Main.java
@@ -1,23 +1,33 @@
 package chapter11;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.URL;
+import java.net.Socket;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		URL url = new URL("http://dokojava.jp/favicon.ico");
-		InputStream is = url.openStream();
-		OutputStream os = new FileOutputStream("dj.ico");;
-		int i = is.read();
-		while(i != -1) {
-			os.write((byte)i);
-			i = is.read();
+	public static void main(String[] args) {
+		Socket sock = null;
+		OutputStream os = null;
+		try {
+			sock = new Socket("smtp.example.com",60025);
+			os = sock.getOutputStream();
+			os.write("HELLO example.com\r\n".getBytes());
+			os.write("MAIL FROM: asaka@example.com\r\n".getBytes());
+			os.write("RCPT TO: minato@example.com\r\n".getBytes());
+			os.write("DATA\r\n".getBytes());
+			os.write("From: asaka@example.com\r\n".getBytes());
+			os.write("Subject: Please send me your RPG\r\n".getBytes());
+			os.write("Hello minato. I would like to play your RPG\r\n".getBytes());
+			os.write("Could you please send it to me ?\r\n".getBytes());
+			os.write(".".getBytes());
+			os.write("QUIT".getBytes());
+			os.flush();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getStackTrace());
+		}finally {
+			try {
+				os.close();
+			}catch(Exception e2) {}
 		}
-		is.close();
-		os.flush();
-		os.close();
 	}
 }
