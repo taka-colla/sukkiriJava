99160b478b2a7c070e6378a10e06e5811bdfa531
takahashi <takahashi@colla-tech.co.jp>
2018-10-29 11:43:31 +0900

    練習 9-2解答

--- sukkiri-java.jissen/src/chapter9/Main.java
+++ sukkiri-java.jissen/src/chapter9/Main.java
@@ -1,21 +1,45 @@
 package chapter9;
-
+import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		FileInputStream fis = new FileInputStream(args[0]);
-		FileOutputStream fos = new FileOutputStream(args[1],true);
-		int i = fis.read();
-		while(i != -1) {
-			fos.write(i);
-			i = fis.read();
+	public static void main(String[] args) {
+		String original = args[0];
+		String copyTo = args[1];
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
+		BufferedOutputStream bos = null;
+		GZIPOutputStream gos =null;
+
+		try {
+			fis = new FileInputStream(original);
+			fos = new FileOutputStream(copyTo);
+			bos = new BufferedOutputStream(fos);
+			gos = new GZIPOutputStream(bos);
+			int i = fis.read();
+			while( i != -1) {
+				gos.write(i);
+				fis.read();
+			}
+			gos.flush();
+			gos.close();
+			fis.close();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getStackTrace());
+			try {
+				if(fis != null) {
+					fis.close();
+				}
+				if(gos != null) {
+					gos.close();
+				}
+			}catch(IOException e2) {}
 		}
-		fos.flush();
-		fos.close();
-		fis.close();
 
 	}
 }
+
