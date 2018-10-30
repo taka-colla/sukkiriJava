54e0d72a8ebae3828e2c7d981148b0f6243e84ab
takahashi <takahashi@colla-tech.co.jp>
2018-10-30 11:54:29 +0900

    練習13-1 解答

---
+++ sukkiri-java.jissen/src/chapter13/Bank.java
@@ -0,0 +1,21 @@
+package chapter13;
+
+
+/**
+ * 銀行を表すクラス。
+ * */
+public class Bank {
+	/** 銀行名 */
+	String name;
+	/** 銀行の住所 */
+	String address;
+	/**
+	 * 口座を作成するメソッド。
+	 * @param owner 口座名義人
+	 * @param initilBalance 初期残高
+	 * */
+	public void addAccount(String owner, int initialBalance) {}
+	public static void main(String[] args) {
+		System.out.println("試験用のメインメソッドです");
+	}
+}
