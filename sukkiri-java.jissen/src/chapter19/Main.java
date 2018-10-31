46522893d48099fc169fb97cc64c0b0b6b954b79
takahashi <takahashi@colla-tech.co.jp>
2018-10-31 18:16:15 +0900

    リスト 19-2

---
+++ sukkiri-java.jissen/src/chapter19/Main.java
@@ -0,0 +1,20 @@
+package chapter19;
+
+import java.util.Scanner;
+
+class PrintingThread extends Thread{
+	public void run() {
+		for(int i = 0; i <10; i++) {
+			System.out.print(i);
+		}
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("なにか入力してください");
+		Thread t = new PrintingThread();
+		t.start();
+		new Scanner(System.in).nextLine();
+	}
+}
