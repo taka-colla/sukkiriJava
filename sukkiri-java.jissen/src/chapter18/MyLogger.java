0ac2802cb726b7d415d1a4eb7c59b46815a85ced
takahashi <takahashi@colla-tech.co.jp>
2018-10-31 17:37:23 +0900

    chapter19 追加

--- sukkiri-java.jissen/src/chapter18/MyLogger.java
+++ sukkiri-java.jissen/src/chapter18/MyLogger.java
@@ -1,5 +1,30 @@
 package chapter18;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MyLogger {
+
+	private static MyLogger theInstance; //唯一のインスタンス保存用
+	private FileWriter writer;
+
+	private MyLogger() {
+		try {
+			this.writer = new FileWriter("c:\\dummlog.txt");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void log(String msg) throws IOException {
+		this.writer.write(msg);
+	}
+
+	public static MyLogger getInstance() {
+		if(theInstance == null) {
+			theInstance = new MyLogger();
+		}
+		return theInstance;
+	}
 
 }
